9ee13b0dfa99c883828375f54ebb9dffb03883f4
Lana Stojcic <ls21750@student.uni-lj.si>
2024-08-06 21:19:40 +0200

    posodobljeno

--- zbiranje_podatkov.py
+++ zbiranje_podatkov.py
@@ -12,10 +12,9 @@ vsebina_strani = html.text
 # Regularni izrazi za iskanje potrebnih informacij
 primeri_službe = re.compile(r'<article class="job-item" data-jobid=.*?>(.*?)</div>', re.DOTALL)
 primer_dela = re.compile(r'<h3>(.*?)</h3>', re.DOTALL)
-primer_kraj = re.compile(r'<div class="job-location">(.*?)</div>', re.DOTALL)
-primer_datum = re.compile(r'<div class="job-date">(.*?)</div>', re.DOTALL)
-primer_opisa = re.compile(r'<div class="job-description">(.*?)</div>', re.DOTALL)
-
+primer_kraj = re.compile(r'</use></svg> (.*?) </p>', re.DOTALL)
+primer_cena = re.compile(r'<strong>.*? €/h neto</strong> (.*? €/h bruto)</a>', re.DOTALL)
+primer_opisa = re.compile(r'<p class="description text-break">(.*?) </p>', re.DOTALL)
 
 # Najdi vse oglase
 službe = primeri_službe.findall(vsebina_strani)
@@ -25,12 +24,12 @@ data = []
 for služba in službe:
     delo = primer_dela.search(služba)
     kraj = primer_kraj.search(služba)
-    datum = primer_datum.search(služba)
+    plača = primer_cena.search(služba)
     opis = primer_opisa.search(služba)
 
     # Preveri, ali so vsi podatki najdeni
-    if delo and kraj and datum and opis:
-        data.append([delo.group(1).strip(), kraj.group(1).strip(), datum.group(1).strip(), opis.group(1).strip()])
+    if delo and kraj and plača and opis:
+        data.append([delo.group(1).strip(), kraj.group(1).strip(), plača.group(1).strip(), opis.group(1).strip()])
 
 # Določi ime CSV datoteke
 csv_file = 'studentska_dela.csv'
@@ -38,7 +37,7 @@ csv_file = 'studentska_dela.csv'
 with open(csv_file, 'w', newline='', encoding='utf-8') as file:
     writer = csv.writer(file)
     # Zapiši glave stolpcev
-    writer.writerow(['delo', 'kraj', 'datum', 'opis'])
+    writer.writerow(['delo', 'kraj', 'plača', 'opis'])
     # Zapiši podatke
     writer.writerows(data)
 
