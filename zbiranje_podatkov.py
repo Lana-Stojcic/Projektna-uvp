41459dbbc3d29c67f4a3527f393b7654387ec12f
Lana Stojcic <ls21750@student.uni-lj.si>
2024-08-07 18:05:34 +0200

    kraj, z prevelikim presledkom

--- zbiranje_podatkov.py
+++ zbiranje_podatkov.py
@@ -26,28 +26,24 @@ oglasi = poisci_vse_oglase(vsebina_strani)
 def podatki_o_delu(oglas):
     primer_dela = r'<h5 class="mb-0">(.*?)</h5>'
     primer_plače = r'<strong>(.*?)</strong>'
+    primer_kraj = r'<svg class="ticon text-primary">.*?</svg>\s*([^<]+)'
     primer_trajanje = r'<li>Trajanje: <strong><!--sse-->(.*?)<!--/sse--></strong></li>'
     delo = re.search(primer_dela, oglas)
     plača_neto = re.search(primer_plače, oglas)
+    kraj = re.search(primer_kraj, oglas)
     trajanje = re.search(primer_trajanje, oglas)
-    if not delo:
+
+    if not delo or not plača_neto or not trajanje or not kraj:
         return None
-    if not plača_neto:
-        return None
-    if not trajanje:
-        return None
-    return {
-        'delo': delo.group(1), 
-        'plača_neto': plača_neto.group(1), 
-        'trajanje': trajanje.group(1)
-    }
+
+    return {'delo': delo.group(1), 'plača_neto': plača_neto.group(1), 'kraj': kraj.group(1), 'trajanje': trajanje.group(1)}
 
 def izpisi_podatke(oglasi):
     data = []
     for oglas in oglasi:
-        details = podatki_o_delu(oglas)
-        if details:  # Only add if details is not None
-            data.append(details)
+        podrobnosti = podatki_o_delu(oglas)
+        if podrobnosti:  # Only add if details is not None
+            data.append(podrobnosti)
             print(f"Extracted details for {len(data)} job ads")
     return data
 data = izpisi_podatke(oglasi)
@@ -58,7 +54,7 @@ csv_file = 'studentska_dela.csv'
 
 # Zapiši podatke v CSV datoteko
 with open(csv_file, 'w', newline='', encoding='utf-8') as file:
-    writer = csv.DictWriter(file, fieldnames=['delo', 'plača_neto', 'trajanje'])
+    writer = csv.DictWriter(file, fieldnames=['delo', 'plača_neto', 'kraj','trajanje'])
     writer.writeheader()
     writer.writerows(data)
 
